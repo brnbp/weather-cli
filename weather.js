3f28f6542e800b21f8a4ef4a487737fc42d8abb3
filipenunes <filipe@lik3.com.br>
2016-10-26 12:40:48 -0200

    add ;

--- weather.js
+++ weather.js
@@ -1,16 +1,16 @@
-const request = require('request')
+const request = require('request');
 
 module.exports = function (location, callback) {
 
-  const apiKey = 'a3ed1549cd8e7a34bd6148a5d1e56a5e'
+  const apiKey = 'a3ed1549cd8e7a34bd6148a5d1e56a5e';
 
-  const encodedLocation = encodeURIComponent(location)
+  const encodedLocation = encodeURIComponent(location);
 
   if (!encodedLocation) {
     return console.log('no location provided')
   }
 
-  const url = `http://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${encodedLocation}&units=metric`;
+  const url = `http://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${encodedLocation}&units=metric`
 
   request({
     url: url,
