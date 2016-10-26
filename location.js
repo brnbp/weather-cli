5d81895334faae6e7497f4d0cc2c359a01ed252f
Bruno Pereira <bruno9pereira@gmail.com>
2016-10-26 15:25:05 -0200

    Update location.js

--- location.js
+++ location.js
@@ -1,13 +1,13 @@
 const request = require('request')
-const url = 'http://ipinfo.io';
+const url = 'http://ipinfo.io'
 
 module.exports = function(callback) {
   request({
     url: url,
     json: true
   }, function(err, res, body) {
-    if (err) throw err;
-    if (!body) return null;
+    if (err) throw err
+    if (!body) return null
     callback({
       city: body.city,
       loc: body.loc
